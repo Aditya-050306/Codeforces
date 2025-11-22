7356c4adcacb2ea2c6412dbdc6ea98d3de4fb4bb
Aditya-050306 <joshiaditya0503@gmail.com>
2025-11-22 19:59:50 +0530

    71A

---
+++ longwords.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int n;
+    string s;
+    cin>>n;
+    while(n--){
+        cin>>s;
+        if(s.length()>10){
+            cout<<s.front()<<s.length()-2<<s.back()<<endl;
+        }
+        else{
+            cout<<s<<endl;
+        }
+    }
+    return 0;
+}
